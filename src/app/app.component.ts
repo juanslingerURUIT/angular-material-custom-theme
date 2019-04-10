b4d51c3dcab5cf2df6c9524289b982d77f45481c
Juan Gabriel Slinger <juanslinger@gmail.com>
2019-04-10 11:24:25 -0300

    First commit with an example of how to use custom theme for angular material. Also add angular material components as Input, Tree, Tabs and Table

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,91 @@
-import { Component } from '@angular/core';
+import { NestedTreeControl } from "@angular/cdk/tree";
+import { Component, OnInit, ViewChild } from "@angular/core";
+import { MatTreeNestedDataSource } from "@angular/material/tree";
+import { MatPaginator, MatSort, MatTableDataSource } from "@angular/material";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
-export class AppComponent {
-  title = 'test-material';
+export class AppComponent implements OnInit {
+  dataSource = new MatTreeNestedDataSource<FoodNode>();
+  title = "test-material";
+  treeControl = new NestedTreeControl<FoodNode>(node => node.children);
+  displayedColumns: string[] = ["position", "name", "weight", "symbol"];
+  dataSourceTable = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
+
+  constructor() {
+    this.dataSource.data = TREE_DATA;
+  }
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+
+  ngOnInit() {
+    this.dataSourceTable.paginator = this.paginator;
+    this.dataSourceTable.sort = this.sort;
+  }
+
+  applyFilter(filterValue: string) {
+    this.dataSourceTable.filter = filterValue.trim().toLowerCase();
+  }
+
+  hasChild = (_: number, node: FoodNode) =>
+    !!node.children && node.children.length > 0;
 }
+
+interface FoodNode {
+  name: string;
+  children?: FoodNode[];
+}
+
+interface PeriodicElement {
+  name: string;
+  position: number;
+  weight: number;
+  symbol: string;
+}
+
+const TREE_DATA: FoodNode[] = [
+  {
+    name: "Fruit",
+    children: [{ name: "Apple" }, { name: "Banana" }, { name: "Fruit loops" }]
+  },
+  {
+    name: "Vegetables",
+    children: [
+      {
+        name: "Green",
+        children: [{ name: "Broccoli" }, { name: "Brussel sprouts" }]
+      },
+      {
+        name: "Orange",
+        children: [{ name: "Pumpkins" }, { name: "Carrots" }]
+      }
+    ]
+  }
+];
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  { position: 1, name: "Hydrogen", weight: 1.0079, symbol: "H" },
+  { position: 2, name: "Helium", weight: 4.0026, symbol: "He" },
+  { position: 3, name: "Lithium", weight: 6.941, symbol: "Li" },
+  { position: 4, name: "Beryllium", weight: 9.0122, symbol: "Be" },
+  { position: 5, name: "Boron", weight: 10.811, symbol: "B" },
+  { position: 6, name: "Carbon", weight: 12.0107, symbol: "C" },
+  { position: 7, name: "Nitrogen", weight: 14.0067, symbol: "N" },
+  { position: 8, name: "Oxygen", weight: 15.9994, symbol: "O" },
+  { position: 9, name: "Fluorine", weight: 18.9984, symbol: "F" },
+  { position: 10, name: "Neon", weight: 20.1797, symbol: "Ne" },
+  { position: 11, name: "Sodium", weight: 22.9897, symbol: "Na" },
+  { position: 12, name: "Magnesium", weight: 24.305, symbol: "Mg" },
+  { position: 13, name: "Aluminum", weight: 26.9815, symbol: "Al" },
+  { position: 14, name: "Silicon", weight: 28.0855, symbol: "Si" },
+  { position: 15, name: "Phosphorus", weight: 30.9738, symbol: "P" },
+  { position: 16, name: "Sulfur", weight: 32.065, symbol: "S" },
+  { position: 17, name: "Chlorine", weight: 35.453, symbol: "Cl" },
+  { position: 18, name: "Argon", weight: 39.948, symbol: "Ar" },
+  { position: 19, name: "Potassium", weight: 39.0983, symbol: "K" },
+  { position: 20, name: "Calcium", weight: 40.078, symbol: "Ca" }
+];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,36 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule } from "@angular/core";
+import {
+  MatButtonModule,
+  MatIconModule,
+  MatInputModule,
+  MatPaginatorModule,
+  MatSortModule,
+  MatTableModule,
+  MatTabsModule,
+  MatTreeModule
+} from "@angular/material";
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatInputModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatTableModule,
+    MatTabsModule,
+    MatTreeModule,
     AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
